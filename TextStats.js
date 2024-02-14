054510c65d321f4440eeff1b53b4a45b72becca7
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-14 20:02:15 +0300

    get number of sentences based on ., !, or ?

--- TextStats.js
+++ TextStats.js
@@ -6,4 +6,20 @@ export class TextStats {
   getNumberOfChars(text) {
     return text.length;
   }
+
+  getNumberOfSentences(text) {
+    let numberOfSentences = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      if (
+        text.charAt(i) === "." ||
+        text.charAt(i) === "?" ||
+        text.charAt(i) === "!"
+      ) {
+        numberOfSentences++;
+      }
+    }
+
+    return numberOfSentences;
+  }
 }
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { TextStats } from "./TextStats";
 const textarea = document.querySelector("textarea");
 const chars = document.querySelector(".chars");
 const words = document.querySelector(".words");
-const sentence = document.querySelector(".sentence");
+const sentences = document.querySelector(".sentence");
 const punctuation = document.querySelector(".punctuation");
 
 const stats = new TextStats();
@@ -11,4 +11,5 @@ const stats = new TextStats();
 textarea.addEventListener("keyup", (event) => {
   words.textContent = stats.getNumberOfWords(event.target.value);
   chars.textContent = stats.getNumberOfChars(event.target.value);
+  sentences.textContent = stats.getNumberOfSentences(event.target.value);
 });
