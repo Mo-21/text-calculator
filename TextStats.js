72ed94fdc4b649fed0411faa8a894ca5553a653a
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-14 20:40:59 +0300

    plot the text stats with Chart.js

--- TextStats.js
+++ TextStats.js
@@ -32,6 +32,51 @@ export class TextStats {
 
     return numberOfPunctuation;
   }
+
+  plotData(text) {
+    const labels = ["chars", "words", "sentences", "punctuations"];
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: "Text Stats",
+          data: [
+            this.getNumberOfChars(text),
+            this.getNumberOfWords(text),
+            this.getNumberOfSentences(text),
+            this.getNumberOfPunctuation(text),
+          ],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 205, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+          ],
+          borderColor: [
+            "rgb(255, 99, 132)",
+            "rgb(255, 159, 64)",
+            "rgb(255, 205, 86)",
+            "rgb(75, 192, 192)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    const config = {
+      type: "bar",
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    };
+
+    return { labels, data, config };
+  }
 }
 
 const punctuation_marks = [
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import { TextStats } from "./TextStats";
+import Chart from "chart.js/auto";
 
 const textarea = document.querySelector("textarea");
 const chars = document.querySelector(".chars");
 const words = document.querySelector(".words");
 const sentences = document.querySelector(".sentence");
 const punctuation = document.querySelector(".punctuation");
+const ctx = document.getElementById("chart");
 
 const stats = new TextStats();
 
@@ -13,4 +15,7 @@ textarea.addEventListener("keyup", (event) => {
   chars.textContent = stats.getNumberOfChars(event.target.value);
   sentences.textContent = stats.getNumberOfSentences(event.target.value);
   punctuation.textContent = stats.getNumberOfPunctuation(event.target.value);
+
+  if (window.ctx != undefined) window.ctx.destroy();
+  window.ctx = new Chart(ctx, stats.plotData(event.target.value).config);
 });
